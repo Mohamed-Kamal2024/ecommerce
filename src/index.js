16866ace4077ff0773da333a68a62ea3d898d8ad
Mohamed <      >
2025-04-13 23:22:55 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+import "bootstrap/dist/css/bootstrap.min.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+import "./css/style.css";
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+console.log('Hello, world!');
+
+console.log('This is a test file.');
---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: { app: "./src/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    hot: false,
+    port: 9000,
+    open: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          minimize: true,
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /bootstrap\.min\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /bootstrap\.min\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          "rtlcss-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "./images/[name][ext]",
+        },
+      },
+      {
+        test: /\.(eot|svg|woff|woff2|ttf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "./fonts/[name][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // Template file
+      filename: "index.html", // Output file
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
+    }),
+    new CssMinimizerPlugin(),
+  ],
+};
