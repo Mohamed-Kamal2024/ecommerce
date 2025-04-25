e772cdbd0d557a34c063516c1d7cfb1a9c00deb8
Mohamed <      >
2025-04-25 10:33:26 +0300

    brand products done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.js");
 import "bootstrap/dist/css/bootstrap.min.css";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import "./css/style.css";
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-console.log('Hello, world!');
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+console.log("Hello, world!");
 
-console.log('This is a test file.');
+document.querySelectorAll(".add-to-cart-btn").forEach((item) => {
+  item.addEventListener("click", () => {
+    alert("!اضيف هذا المنتج الي عربه الشراء");
+  });
+});
+
